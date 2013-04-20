51aed438ccb2337786b9218034f5066ac93c677e
Wills Bithrey <wills@potatolondon.com>
2013-04-20 17:02:52 +0100

    Fixing basic layout

--- Samples/Facebook/js/main.js
+++ Samples/Facebook/js/main.js
@@ -1,1 +1,8 @@
-/**/
+$(document).ready(function() {
+	preventOverScroll(document.querySelector('.scroll-root'));
+
+	// Set correct height on the content pane
+	$('.content-wrapper').css({
+		height: $(window).height()
+	});
+});
